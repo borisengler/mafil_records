5d07b16f0accce1e559c40a3a451c1403203ecab
Boris Engler <engler@ginasystem.com>
2024-01-27 17:59:24 +0100

    fail

--- mafil-records-app-1.0.0/frontend/src/utils/RemoveSeriesFromLocalStorage.tsx
+++
@@ -1,6 +0,0 @@
-function removeSeriesFromLocalStorage() {
-  const seriesKeys = Object.keys(localStorage).filter((key) => key.startsWith('series-'));
-  seriesKeys.forEach((key) => localStorage.removeItem(key));
-}
-
-export default removeSeriesFromLocalStorage;
